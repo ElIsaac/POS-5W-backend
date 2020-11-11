6173d6e131a682218a72fc94fd91a75f74fe3af2
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-10 19:22:37 -0600

    coneccion a mongodb creada

---
+++ bd.js
@@ -0,0 +1,8 @@
+
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://localhost/tiendita',{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(db => console.log('base de datos conectada')).catch(err => console.log(err));
