02d4b6004030bc74f69b0b8680f58f182f6d51c6
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-10 19:21:56 -0600

    index.js creado

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+require('./bd')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 4000);
+
+app.use(cors());
+app.use(require('./routes/usuarioComun'));
+app.use(require('./routes/usuarioAdmin'));
+app.use(require('./routes/usuarios'));
+
+app.listen(app.get('port'), ()=>{
+    console.log("servidor en puerto: ",app.get('port'));
+})
