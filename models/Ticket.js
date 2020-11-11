5e67ce464f04a20abd6837a3e0b9e245f44083a7
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-10 19:23:34 -0600

    modelo de datos a guardar en mongodb (productos, tickets y usuarios)

---
+++ models/Ticket.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose")
+
+
+const TicketSchema = new Schema({
+    productos: [{
+       type: Object, 
+       required: true 
+    }],
+    precioFinal: { 
+      type: Number, 
+      required: true 
+    },
+    idCajero: {
+        type: String, 
+        required: true 
+     },
+    nombreCajero: {
+       type: String, 
+       required: true 
+    },
+    fecha: { 
+      type: Date, 
+      default: Date.now 
+    }
+  });
+
+module.exports= model("Ticket", TicketSchema)
---
+++ models/Usuario.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+
+/////////////////modelo de datos que se guardaran en mongodb///////
+const UsuarioSchema = new Schema({
+  nombre: {
+     type: String, 
+     required: true 
+  },
+  email: { 
+    type: String, 
+    required: true,
+    unique: true
+  },
+  contrasenia: {
+     type: String, 
+     required: true 
+  },
+  fecha: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+////////////////////FUncion asincrona para encriptar la contraseña(se antes de usar el modelo)//////
+UsuarioSchema.methods.encriptar = async contrasenia => {
+  const salt = await bcrypt.genSalt(10);////gen salt(10) es el parametro que se da para que tan fuerte sara la encriptacion
+  return await bcrypt.hash(contrasenia, salt);
+};
+
+/////////////////////este metodo es para encriptar la contraseña del inicio de sesion 
+/////////////y compararla con la contraseña encriptada original
+UsuarioSchema.methods.matchPassword = async function(contrasenia) {
+  return await bcrypt.compare(contrasenia, this.contrasenia);
+};
+
+module.exports = model("Usuario", UsuarioSchema);
