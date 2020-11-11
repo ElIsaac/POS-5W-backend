1f92159fd7c3f4804844a3ee44fdfcf8bb310d32
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-10 20:13:46 -0600

    warning de dependencia deprecada corregido

--- bd.js
+++ bd.js
@@ -4,5 +4,6 @@ const mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost/tiendita',{
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true, 
+    useCreateIndex: true
 }).then(db => console.log('base de datos conectada')).catch(err => console.log(err));
